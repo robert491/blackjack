544015e2b49d59334f1b75050368ef91e8aa560a
robert491 <bob.pototsky@gmail.com>
2020-11-20 23:59:46 +0300

    hand methods

--- hand.rb
+++ hand.rb
@@ -3,10 +3,27 @@ class Hand
 
   def initialize(cards)
     @cards = cards
+    @value = 0
   end
 
   def value
-    @cards.
+    aces = 0
+    @cards.each do |card|
+      if numeric?(card)
+        @value += card.value
+      elsif face?(card)
+        @value += 10
+      else
+        aces += 1
+      end
+  end
+
+  def face?(card)
+    ['J', 'Q', 'K'].include?(card.value)
+  end
+
+  def numeric?(card)
+    card.value.is_a? Numeric
   end
 
   def to_s
